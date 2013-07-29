0901913262317eb18c98ca86b27370d9ce1accb1
Jason Frame <jason@onehackoranother.com>
2013-07-29 13:52:47 +0100

    second

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 function clamp(min, max, v) {
-	if (v < min) return min;
+  if (v < min) return min;
   if (v > max) return max;
   return v;
 }
 
 function clamper(min, max) {
-	return function(v) {
-		return clamp(min, max, v);
-	}
+  return function(v) {
+    return clamp(min, max, v);
+  }
 }
 
 function lerp(x, y, r) {
-	return x + ((y - x) * r);
+  return x + ((y - x) * r);
 }
 
 function lerper(x, y) {
-	return function(r) {
-		return lerp(x, y, r);
-	}
+  return function(r) {
+    return lerp(x, y, r);
+  }
 }
 
 function expose() {
-  Math.clamp    = clamp;
-  Math.clamper  = clamper;
-  Math.lerp     = lerp;
-  Math.lerper   = lerper;
+  for (var k in module.exports) {
+    if (k === 'expose')
+      continue;
+    Math[k] = module.exports[k];
+  }
 }
 
 module.exports = {
-  clamp 	: clamp,
-  clamper	: clamper,
-  lerp 		: lerp,
-  lerper	: lerper,
+  clamp   : clamp,
+  clamper : clamper,
+  lerp    : lerp,
+  lerper  : lerper,
 
-  expose	: expose
+  expose  : expose
 };
